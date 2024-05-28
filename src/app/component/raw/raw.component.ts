1cb38bd63affe6d19c2a67dc58833b8245decfe2
Benja-san <beugnet.freelance@gmail.com>
2024-05-28 18:42:23 +0200

    Add color pattern and style injection for raws refactor header & footer architecture

--- src/app/component/raw/raw.component.ts
+++ src/app/component/raw/raw.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { Raw } from '../../model/raw/raw';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-raw',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './raw.component.html',
   styleUrl: './raw.component.scss',
 })
 export class RawComponent {
-  @Input() title!: string;
+  @Input() raw!: Raw;
 }
