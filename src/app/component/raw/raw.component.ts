4ad1ecf41c7acd394df83206e6c7d7b6215e9c87
Benja-san <beugnet.freelance@gmail.com>
2024-06-11 18:23:53 +0200

    Add edition for raw title Add responsive css add click button for toggle raw edit mode

--- src/app/component/raw/raw.component.ts
+++ src/app/component/raw/raw.component.ts
@@ -1,14 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Raw } from '../../model/raw/raw';
 import { NgStyle } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-raw',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, FormsModule],
   templateUrl: './raw.component.html',
   styleUrl: './raw.component.scss',
 })
 export class RawComponent {
   @Input() raw!: Raw;
+  isEditing: boolean = false;
+
+  openEditMode() {
+    this.isEditing = !this.isEditing;
+  }
 }
